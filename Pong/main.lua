475738dcfcb5ba24769fe681cc6d3441e12eab20
Paganini-Thurler <127864316+Paganini-Thurler@users.noreply.github.com>
2026-04-21 21:07:24 -0300

    Create main.lua
    HelloWorld program on LUA with LÖVE.

---
+++ Pong/main.lua
@@ -0,0 +1,16 @@
+-- Global variables --
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+function love.load() 
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT,{
+        resizable = false, vsync = true, fullscreen = false
+    })
+
+end 
+
+function love.draw()
+    -- The -6 is to account for the font height
+    love.graphics.printf("Hello!", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, "center")
+end
