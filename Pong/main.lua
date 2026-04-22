66fe1fcd8ddb452a1185308d36e14e956dccbeef
Paganini-Thurler <127864316+Paganini-Thurler@users.noreply.github.com>
2026-04-21 21:52:09 -0300

    Virtual canvas added
    Added a virtual canvas library.

--- Pong/main.lua
+++ Pong/main.lua
@@ -1,16 +1,53 @@
+-- Imports
+
+--[[
+    push is a library that allows us to draw our game at a virtual
+    resolution, used to provide a more retro aesthetic
+]]
+
+push = require "push"
+
 -- Global variables --
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+-- Notes:
+-- In Lua code blocks, such as conditionals ends with end
+
+
+-- Setup function
 function love.load() 
+    -- Graphical mode
+    love.graphics.setDefaultFilter("nearest", "nearest")
+
+    -- Creates a window 
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT,{
         resizable = false, vsync = true, fullscreen = false
     })
 
+    -- Creates a virtual resolution screen
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, {upscale = "normal"})
+
 end 
 
+-- Keyboard input
+function love.keypressed(key)
+    
+    if key == "escape" then
+        love.event.quit()
+    end
+end
+
+-- The drawing function
 function love.draw()
-    -- The -6 is to account for the font height
-    love.graphics.printf("Hello!", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, "center")
+    -- Starts the virtual screen
+    push.start()
+    -- Draws text in the window, the -6 is to account for the font height
+    love.graphics.printf("Hello!", 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, "center")
+    --End the virtual screen
+    push.finish()
 end
