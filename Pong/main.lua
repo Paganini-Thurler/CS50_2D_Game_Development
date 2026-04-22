9c178c7b14e264828e3e1cc74a9462a88b8bd3d4
Paganini-Thurler <127864316+Paganini-Thurler@users.noreply.github.com>
2026-04-22 19:01:32 -0300

    Field graphics and scores implemented

--- Pong/main.lua
+++ Pong/main.lua
@@ -15,14 +15,24 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PLAYER1_SCORE = 0
+PLAYER2_SCORE = 0
+
+
 -- Notes:
 -- In Lua code blocks, such as conditionals ends with end
 
 
--- Setup function
+-- A Setup function is a function that prepares the environment  
 function love.load() 
+   
     -- Graphical mode
     love.graphics.setDefaultFilter("nearest", "nearest")
+
+    -- Fonts 
+    largeFont = love.graphics.newFont("font.ttf", 32)
+    smallFont = love.graphics.newFont("font.ttf", 8)
+
 
     -- Creates a window 
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT,{
@@ -36,7 +46,6 @@ end
 
 -- Keyboard input
 function love.keypressed(key)
-    
     if key == "escape" then
         love.event.quit()
     end
@@ -46,8 +55,26 @@ end
 function love.draw()
     -- Starts the virtual screen
     push.start()
+    -- Draws a solid color, values are normalized because GPUs use floats
+    love.graphics.clear(40/255, 45/255, 52/255, 1)
+    
+    -- Text
+    -- Sets the font
+    love.graphics.setFont(largeFont)
     -- Draws text in the window, the -6 is to account for the font height
-    love.graphics.printf("Hello!", 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, "center")
+    --love.graphics.printf("Hello!", 0, VIRTUAL_HEIGHT / 2 - 16, VIRTUAL_WIDTH, "center")
+    
+    -- Score
+    love.graphics.print(tostring(PLAYER1_SCORE), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 2 - 80)
+    love.graphics.print(tostring(PLAYER2_SCORE), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 2 - 80)
+    -- Graphics
+    -- Paddle
+    love.graphics.rectangle("fill", 20, 20, 5, 20)
+    -- Paddle 2
+    love.graphics.rectangle("fill", VIRTUAL_WIDTH - 20, 20,  5, 20)
+    -- Ball
+    love.graphics.rectangle("fill", VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
     --End the virtual screen
     push.finish()
 end
