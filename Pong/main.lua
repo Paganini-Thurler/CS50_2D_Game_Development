0f3815134ea6564774813f3eeb513fc7c3e1af39
Paganini-Thurler <127864316+Paganini-Thurler@users.noreply.github.com>
2026-04-26 21:56:50 -0300

    Update main.lua

--- Pong/main.lua
+++ Pong/main.lua
@@ -20,29 +20,24 @@ require "Ball"
 -- Real screen/canvas
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
-
 -- Virtual screen/canvas
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
-
 -- Defines a paddle speed of 200px/s 
 PADDLE_SPEED = 200
-
--- Notes:
--- In Lua code blocks, such as conditionals ends with end
 
 -- A Setup function is a function that prepares the environment.
 function love.load() 
     -- Seed for the random number generator
     -- Created a unique seed based on UNIX time
     math.randomseed(os.time())
-
+    
     -- Graphical mode
     love.graphics.setDefaultFilter("nearest", "nearest")
-
+    
     -- Screen title 
     love.window.setTitle("Pong")
-
+    
     -- Fonts 
     largeFont = love.graphics.newFont("font.ttf", 32)
     smallFont = love.graphics.newFont("font.ttf", 8)
@@ -68,96 +63,47 @@ function love.load()
     -- Sets who will serve 
     servingPlayer = 1
 
-
     -- Ball initial position
     ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
-   gameState = "start"
+    -- Set initial gameState
+   setGameState("start")
 end 
 
 -- Keyboard input
-function love.keypressed(key)
+function love.keypressed(key) 
+    -- Confirm choices by pressing Enter
+    pressEnter(key)
     -- Quits the game
-    if key == "escape" then
-        love.event.quit()
-    end
-
-    if key == "enter" or key == "return" then
-        if gameState == "start"  or gameState == "serving" then
-            gameState = "play"
-        else 
-            gameState = "start"
-            -- Reset ball
-            ball:reset(servingPlayer)
-        end
-    end
+    quitGame(key)
 end
 
 -- The update function is a function that update all the variables before
 -- they can be used to update the graphics based on delta time to make all
 -- the calculations independent of refresh rates or faster CPUS/GPUS
 function love.update(dt)
-    -- Player 1 movement
-    if love.keyboard.isDown("w") then 
-        player1:up()
-    elseif love.keyboard.isDown("s") then
-        player1:down()
-    else
-        player1:stop()
-    end
-
-    -- Player 2 input
-    if love.keyboard.isDown("up") then 
-        player2:up()
-    elseif love.keyboard.isDown("down") then
-        player2:down()
-    else 
-        player2:stop()
-    end
-
+    --  Updates the player position after the input
+    playerMovement(player1, "w", "s")
+    playerMovement(player2, "up", "down")
+   
     -- Ball position update
     if gameState == "play" then
-
-        local playerWhoScored = ball:scores(VIRTUAL_WIDTH)
-
-        if playerWhoScored == 1 then
-            servingPlayer = 2 
-            player1Score = player1Score + 1
-            ball:reset(servingPlayer)
-            gameState = "serving"
-        end 
-
-        if playerWhoScored == 2 then
-            servingPlayer = 1 
-            player2Score = player2Score + 1
-            ball:reset(servingPlayer)
-            gameState = "serving"
-        end 
-        
-
-        if ball:collides(player1) then
-            -- Inverts the x direction from the player 1 
-            ball:changeDX(1)
-            -- Randomizes the ball dy 
-            ball:changeDY()
-        end
-
-        if ball:collides(player2) then
-            -- Inverts the x direction from the player 2 
-            ball:changeDX(2)
-            -- Randomizes the ball dy 
-            ball:changeDY()
-        end
+       -- Checks and sets who will serve
+       whoServes()
+       -- Check the ball collision with a player paddle
+       ballPlayerCollision()
         -- If theres a bounce it will account it
         ball:bounces(VIRTUAL_HEIGHT)
-
         ball:update(dt)
-
     end
+
     -- Update player 1 paddle
     player1:update(dt)
     -- Update player 2 paddle
     player2:update(dt)
+
+    -- Check if the match is over
+    isMatchOver()
 end
 
 -- After the variables are updated by the user input, they are used to
@@ -175,18 +121,27 @@ function love.draw()
     --love.graphics.printf("Hello!", 0, VIRTUAL_HEIGHT / 2 - 16, VIRTUAL_WIDTH, "center")
     
     -- Graphics
-
     -- Score
     love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 2 - 80)
     love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 2 - 80)
-    
+
+    love.graphics.setFont(smallFont)
+    if gameState == "start" then
+        love.graphics.printf("Press Enter to Play",0, VIRTUAL_HEIGHT / 2 - 4, VIRTUAL_WIDTH, "center")
+    elseif gameState == "serving" then
+        love.graphics.printf("Player " .. tostring(servingPlayer) .. " is serving" ,0, VIRTUAL_HEIGHT / 2 + 4, VIRTUAL_WIDTH, "center")
+        love.graphics.printf("Press Enter to Continue",0, VIRTUAL_HEIGHT / 2 - 4, VIRTUAL_WIDTH, "center")
+    end
+
+    if gameState == "play" then
+        ball:render()
+    end
+
     -- Paddle 1
     player1:render(dt)
     -- Paddle 2
     player2:render(dt)
-    -- Ball
-   ball:render(dt)
-
+    
    displayFPS()
 
     --End the virtual screen
@@ -198,7 +153,85 @@ function displayFPS()
     love.graphics.setFont(smallFont)
     -- The RGBA color is Green
     love.graphics.setColor(0,1,0,1)
+    -- .. operator for string concatenation
     love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), 10, 10)
     love.graphics.setColor(1,1,1,1)
 end
-    
+
+function quitGame(key)
+     -- Quits the game
+    if key == "escape" then
+        love.event.quit()
+    end
+end 
+
+-- 
+function pressEnter(key)
+    if key == "enter" or key == "return" then
+        if gameState == "start"  or gameState == "serving" then
+            setGameState("play") 
+        else 
+            setGameState("start")
+            -- Reset ball
+            ball:reset(servingPlayer)
+        end
+    end
+end 
+
+-- Update player paddle movement based on input
+function playerMovement(player, upKey, downKey)
+    -- Player movement
+    if love.keyboard.isDown(upKey) then 
+        player:up()
+    elseif love.keyboard.isDown(downKey) then
+        player:down()
+    else
+        player:stop()
+    end
+end 
+
+-- Check  who will serve the ball
+function whoServes()
+    local playerWhoScored = ball:scores(VIRTUAL_WIDTH)
+    if playerWhoScored == 1 then
+        servingPlayer = 2 
+        player1Score = player1Score + 1
+        ball:reset(servingPlayer)
+        gameState = "serving"
+    end 
+
+    if playerWhoScored == 2 then
+        servingPlayer = 1 
+        player2Score = player2Score + 1
+        ball:reset(servingPlayer)
+        gameState = "serving"
+    end 
+end
+
+-- Checks player ball collision
+function ballPlayerCollision()
+    if ball:collides(player1) then
+        -- Inverts the x direction from the player 1 
+        ball:changeDX(1)
+        -- Randomizes the ball dy 
+        ball:changeDY()
+    end
+
+    if ball:collides(player2) then
+        -- Inverts the x direction from the player 2 
+        ball:changeDX(2)
+        -- Randomizes the ball dy 
+        ball:changeDY()
+    end
+end
+
+function isMatchOver()
+    if player1Score == 5 or player2Score == 5 then
+        gameState = "end"
+        love.graphics.printf("Macth is over",0, VIRTUAL_HEIGHT / 2 - 4, VIRTUAL_WIDTH, "center")
+    end
+end
+
+function setGameState(state)
+    gameState = state
+end
