6847c62d2b6e08edc369202049e324885e4edb81
Paganini-Thurler <127864316+Paganini-Thurler@users.noreply.github.com>
2026-06-16 22:48:47 -0300

    Update Pair.lua

--- Flappy Bird/Pair.lua
+++ Flappy Bird/Pair.lua
@@ -4,9 +4,10 @@
 
 Pair = class{}
 
--- The gap size changes the dificult of the game
+-- The gap size in pixels changes the dificult of the game
 local GAP_HEIGHT = 90
 
+-- Constructor
 function Pair:init(y)
     -- Initializes the pair past the visible screen by 32 pixels
     self.x = VIRTUAL_WIDTH + 32
@@ -17,27 +18,32 @@ function Pair:init(y)
     -- Instantiates the pipe pair
     self.pipes = {
         ["upper"] = Pipe("top", self.y),
-        ["lower"] = Pipe("bottom", self.y + PIPE_HEIGHT + GAPHEIGHT)
+        ["lower"] = Pipe("bottom", self.y + PIPE_HEIGHT + GAP_HEIGHT)
     }
 
     -- Removoval flag
     self.remove = false
 end
 
+-- Updates the Pair
 function Pair:update(dt)
-    removePair()
+    self:removePair(dt)
 end
 
-function removePair()
+--Removes the pair when it goes offscreen
+function Pair:removePair(dt)
+    -- If the position x is less than the - pipe width (offscreen)
     if self.x > -PIPE_WIDTH then
         self.x = self.x - PIPE_SPEED * dt
         self.pipes["lower"].x = self.x
         self.pipes["upper"].x = self.x
     else
+        -- Sets the removal flag to true
         self.remove = true
     end
 end
 
+-- Renders each pipe by calling their render() method
 function Pair:render()
     for key, pipe in pairs(self.pipes) do
         pipe:render()
