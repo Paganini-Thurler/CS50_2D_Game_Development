03961c9366cb53887c05990d888103a1ffee0db4
Paganini-Thurler <127864316+Paganini-Thurler@users.noreply.github.com>
2026-06-17 23:27:50 -0300

    Create TitleScreen.lua

---
+++ Flappy Bird/states/TitleScreen.lua
@@ -0,0 +1,23 @@
+--[[
+    Title Screen State class
+]]
+
+-- Inherits the BaseState
+TitleScreenState = class{__includes = BaseState}
+
+function TitleScreenState:update(dt)
+    -- The trigger to play is enter or space
+    if love.keyboard.wasPressed("enter") or love.keyboard.wasPressed("space") then
+        gameStateMachine:change("play")
+    end
+end 
+
+function TitleScreenState:render()
+    -- Defines the font name not the file.tff
+    love.graphics.setFont(flappyFont)
+    -- Defines 
+    love.graphics.printf("Flappy Bird", 0, 64, VIRTUAL_WIDTH, "center")
+
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf("Press ENTER", 0, 100, VIRTUAL_WIDTH, "center")
+end 
